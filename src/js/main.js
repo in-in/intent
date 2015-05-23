a26f22f386d385a0b0463c588f3c918e46616b67
in-in <timothybernerslee@gmail.com>
2015-05-24 02:17:52 +0300

    add filter menu

--- src/js/main.js
+++ src/js/main.js
@@ -11577,29 +11577,24 @@ return jQuery;
 ;
 $(document).ready(function () {
   'use strict';
-  $('.slider').on({
-    beforeChange: function (event, slick, current_slide_index, next_slide_index) {
-      $('.thumbnails .slick-slide').removeClass('slick-main-active');
-      $('.thumbnails .slick-slide[data-index=' + next_slide_index + ']').addClass('slick-main-active');
-    }
-  }).slick({
+  $('.slider').slick({
     asNavFor: '.thumbnails',
+    slideToShow: 1,
+    slidesToScroll: 1,
     lazyLoad: 'ondemand',
+    dots: false,
+    arrows: true,
     speed: 400,
     fade: true,
     infinite: true,
-    slideToShow: 1,
-    slidesToScroll: 1,
-    dots: false,
-    arrows: true,
     appendArrows: $('.slider-arrows'),
     prevArrow: '<svg class="icon-arrow"><use xlink:href="#icon-arrow-left"></use></svg>',
     nextArrow: '<svg class="icon-arrow" ><use xlink:href="#icon-arrow-right"></use></svg>'
   });
   $('.thumbnails').slick({
+    asNavFor: '.slider',
     slidesToShow: 10,
     slidesToScroll: 10,
-    asNavFor: '.slider',
     dots: false,
     arrows: false,
     focusOnSelect: true

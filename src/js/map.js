cb202e948dde754e1c684295525dcdc2c45c4266
in-in <timothybernerslee@gmail.com>
2015-06-15 20:41:45 +0300

    add map

---
+++ src/js/map.js
@@ -0,0 +1,21 @@
+var map;
+function initialize() {
+  'use strict';
+  
+  var myLatlng = new google.maps.LatLng(52.135610, -106.635511);
+
+  var mapOptions = {
+    zoom: 14,
+    center: myLatlng
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+  
+  var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(52.135024, -106.647645),
+      map: map
+    });
+
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
